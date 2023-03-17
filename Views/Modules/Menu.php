44e44c09aeab31b385f456354187f9483c61c833
unknown <marcko236@gmail.com>
2023-03-17 16:16:55 -0500

    Se añade y configura platilla Ela-Admin

--- Views/Modules/Menu.php
+++ Views/Modules/Menu.php
@@ -1,1 +1,23 @@
-<h1>Menu</h1>
+<!-- Left Panel -->
+<aside id="left-panel" class="left-panel">
+        <nav class="navbar navbar-expand-sm navbar-default">
+            <div id="main-menu" class="main-menu collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active">
+                        <a href="home"><i class="menu-icon fa fa-laptop"></i>Inicio</a>
+                    </li>
+                    <li class="menu-title">Opciones</li><!-- /.menu-title -->
+                    <li class="menu-item-has-children dropdown">
+                        <a href="products" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-cogs"></i>Productos</a>
+                        <ul class="sub-menu children dropdown-menu">                            <li><i class="fa fa-puzzle-piece"></i><a href="ui-buttons.html">Buttons</a></li>
+                            <li><i class="fa fa-id-badge"></i><a href="productos">Productos</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children dropdown">
+                        <a href="profile"><i class="menu-icon fa fa-table"></i>Perfil</a>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </nav>
+    </aside>
+    <!-- /#left-panel -->
--- Views/Template.php
+++ Views/Template.php
@@ -5,15 +5,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Template</title>
+    <!-- estilos de la plantilla ELA-->
+    <link rel="apple-touch-icon" href="Views/Img/Icons/icon.png">
+    <link rel="shortcut icon" href="Views/Img/Icons/icon.png">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
+    <link rel="stylesheet" href="Views/assets/css/cs-skin-elastic.css">
+    <link rel="stylesheet" href="Views/assets/css/style.css">
+    <!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/html5shiv/3.7.3/html5shiv.min.js"></script> -->
+    <link href="https://cdn.jsdelivr.net/npm/chartist@0.11.0/dist/chartist.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/jqvmap@1.5.1/dist/jqvmap.min.css" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/weathericons@2.1.0/css/weather-icons.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.css" rel="stylesheet" />
+    <!-- fin estilos -->
 </head>
 <body>
 
     <?php 
-        include "Modules/head.php";
-
+        
         include "Modules/menu.php";
 
-        include "Modules/footer.php";
+        echo '<div id="right-panel" class="right-panel">';
+        include "Modules/head.php";
 
         //Friendly URL
         if(isset($_GET["ruta"])){
@@ -23,6 +42,9 @@
                     include "Modules/".$_GET["ruta"].".php";
                 }
         }
+        
+        include "Modules/footer.php";
+        echo '</div>';
     ?>
     
 </body>
