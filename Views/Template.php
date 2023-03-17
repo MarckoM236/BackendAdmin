0c9dc45b2150d81afdcb122d777bc264b67d1ecb
unknown <marcko236@gmail.com>
2023-03-17 11:35:09 -0500

    Creacion y estructura del proyecto

---
+++ Views/Template.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template</title>
+</head>
+<body>
+
+    <?php 
+        include "Modules/Head.php";
+
+        include "Modules/Menu.php";
+
+        include "Modules/Footer.php";
+    ?>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+
+require_once "Controllers/TemplateController.php";
+
+$template=new TemplateController();
+$template->CtrTemplate();
